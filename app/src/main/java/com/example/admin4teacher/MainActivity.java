97f6ac21f3cb39f8da691bf0ac033462ad4082d5
Pablo Javier Alemán Velásquez <paleman-2019222@kinal.edu.gt>
2023-05-24 02:05:04 -0600

    conexion con la base de datos

--- app/src/main/java/com/example/admin4teacher/MainActivity.java
+++ app/src/main/java/com/example/admin4teacher/MainActivity.java
@@ -3,12 +3,88 @@ package com.example.admin4teacher;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.JsonRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity implements Response.Listener<JSONObject>, Response.ErrorListener {
+    RequestQueue rq;
+    JsonRequest jrq;
+
+    TextInputEditText userTxt, passTxt;
+    Button btnLogin;
+
+    TextView label;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userTxt = (TextInputEditText) findViewById(R.id.txtUser);
+        passTxt=(TextInputEditText) findViewById(R.id.txtPassword);
+        label=(TextView) findViewById(R.id.textView1);
+        btnLogin=(Button)  findViewById(R.id.btnLogin);
+        rq = Volley.newRequestQueue(getApplicationContext());
+        btnLogin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                iniciarSesion();
+            }
+        });
     }
+
+    @Override
+    public void onErrorResponse(VolleyError error) {
+        Toast.makeText(getApplicationContext(), "No se encontró el error " + error.toString(), Toast.LENGTH_SHORT).show();
+        error.printStackTrace();
+        Log.i("ERROR", error.toString());
+        label.setText(error.toString());
+    }
+
+    @Override
+    public void onResponse(JSONObject response) {
+        User usuario = new User();
+        Toast.makeText(getApplicationContext(), "Se ha encontrado el usuario " + userTxt.getText().toString(), Toast.LENGTH_SHORT).show();
+
+        JSONArray jsonArray = response.optJSONArray("datos");
+        JSONObject jsonObject= null;
+
+        try {
+            jsonObject = jsonArray.getJSONObject(0);
+            usuario.setUser(jsonObject.optString("user"));
+            usuario.setName(jsonObject.optString("name"));
+            usuario.setLastname(jsonObject.optString("lastname"));
+            usuario.setPassword(jsonObject.optString("pwd"));
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+
+        
+
+    }
+
+    private void iniciarSesion(){
+        String url = "http://192.168.1.2/bdadmin4teacher/sesion.php?user="+userTxt.getText().toString()+
+                "&pwd="+passTxt.getText().toString();
+        jrq = new JsonObjectRequest(Request.Method.GET, url, null, this, this);
+        rq.add(jrq);
+
+    }
+
 }
---
+++ app/src/main/java/com/example/admin4teacher/User.java
@@ -0,0 +1,62 @@
+package com.example.admin4teacher;
+
+public class User {
+    private int idUser;
+    private String name, lastname, email, phone, user, password;
+
+    public int getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(int idUser) {
+        this.idUser = idUser;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
