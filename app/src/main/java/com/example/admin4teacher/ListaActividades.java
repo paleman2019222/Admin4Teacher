2c2e515e38cf597ebdb900a5edfa8cda098848da
Petter99 <mcanil1999@gmail.com>
2023-06-07 05:38:55 -0600

    creo clase para tareas

---
+++ app/src/main/java/com/example/admin4teacher/ListaActividades.java
@@ -0,0 +1,14 @@
+package com.example.admin4teacher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListaActividades extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_actividades);
+    }
+}
